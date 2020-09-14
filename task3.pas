96282b174a18716b7e5b923edaa7f2ae966bc4f8
BH <vvtretyakov18@gmail.com>
2020-09-14 19:29:55 +0400

    Comment was added

---
+++ task3.pas
@@ -0,0 +1,16 @@
+unit task2;
+
+interface
+
+uses
+  Windows, Forms;
+
+implementation
+
+procedure aaa;
+begin
+  MessageBox(Application.Handle, PChar('Comment was added'), PChar('Comment was added'), MB_OK + MB_ICONINFORMATION);
+end;
+
+end.
+ 
